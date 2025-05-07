7d59ccf45a08495f249a68502cbe6e66155f5a31
hoojo_@126.com <hoojo_@126.com>
2025-05-07 15:31:17 +0800

    feat: 创建 Vue 3 项目模板
    - 添加项目结构和基本配置
    - 创建 App.vue 和其他组件
    - 设置路由和全局样式
    - 添加图标和欢迎页面- 配置 VSCode 和 ESLint

---
+++ create-app/es-module.vue
@@ -0,0 +1,16 @@
+<script type="module">
+  import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+  createApp({
+    setup() {
+      const message = ref('Hello World');
+      return {
+        message
+      };
+    }
+  }).mount("#app");
+</script>
+
+<div id="app">
+    {{ message }}
+</div>
