1a0f3af5765c1669c01c3e53538a8321dc4d50e4
hoojo_@126.com <hoojo_@126.com>
2025-05-26 14:58:34 +0800

    test(app): 添加 Vitest 测试配置和示例测试文件
    - 在 package.json 中添加 test脚本，使用 Vitest 运行测试
    - 新增 vue.test.js 文件，包含多个测试用例
    - 测试内容包括基本数学运算、对象属性检查等

---
+++ 04.component/app/src/tests/vue.test.js
@@ -0,0 +1,34 @@
+import { expect, test, describe } from 'vitest'
+
+const sum = (a, b) => a + b;
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+});
+
+test('should work as expected', () => {
+    expect(Math.sqrt(4)).toBe(2)
+});
+
+test('这是一个测试', () => {
+    console.log('这是一个测试，xxxx');
+});
+
+const person = {
+    isActive: true,
+    age: 32,
+}
+
+describe('person', () => {
+    test('person is defined', () => {
+        expect(person).toBeDefined()
+    })
+
+    test('is active', () => {
+        expect(person.isActive).toBeTruthy()
+    })
+
+    test('age limit', () => {
+        expect(person.age).toBeLessThanOrEqual(32)
+    })
+})
