9c6a21528ab1b358847948ebd8001322d64d50ba
hoojo_@126.com <hoojo_@126.com>
2025-05-29 10:41:12 +0800

    feat(router): 添加路由守卫和生命周期钩子
    - 在 NotFoundView 组件中添加路由守卫和生命周期钩子
    - 在 main.js 中添加全局前置和后置守卫
    - 优化 Home路由的独享守卫- 添加路由解析流程的注释

--- 08.plugins/router/src/components/NotFoundView.vue
+++ 08.plugins/router/src/components/NotFoundView.vue
@@ -1,8 +1,27 @@
-<script setup>
+<script>
+export default {
+    name: 'notfound',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    beforeRouterEnter(to, from, next) {
+        console.log('beforeRouterEnter', to, from);
+        next();
+    },
+    beforeRouterUpdate(to, from) {
+        console.log('beforeRouterUpdate', to, from);
+    },
+    beforeRouteLeave(to, from) {
+        console.log('beforeRouteLeave', to, from);
+    }
+}
 </script>
 
 <template>
     <div>Not Found page</div>
+    <div>{{ msg }}</div>
 </template>
 
 <style scoped>
--- 08.plugins/router/src/main.js
+++ 08.plugins/router/src/main.js
@@ -16,9 +16,11 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomeView,
-            // 独享守卫
+            // 独享守卫，beforeEnter: [func, func2] 支持多个回调
+            // 父路由和子路由都支持 独享守卫
             beforeEnter: (to, from) => {
                 console.log('Home => beforeEnter', to, from);
+                return true;
             }
         },
         { path: '/user/:id', component: UserView },
@@ -69,7 +71,7 @@ const router = createRouter({
         { path: '/props/:id', component: PropsView, props: true },
         { path: '/props/flag', component: PropsView, props: {flag: true} },
         { path: '/props/search', component: PropsView, props: route => ({query: route.query.q}) },
-    ]
+    ],
 });
 
 // 全局前置守卫
@@ -97,10 +99,30 @@ router.beforeResolve((to, from, next) => {
 
 // 全局后置守卫
 router.afterEach((to, from, failure) => {
-    console.log('afterEach', to, from, failure);
+    console.log('afterEach', to, from);
+    if (failure) {
+        console.log('failure', failure);
+    }
 });
 
 createApp(App)
 .provide("global", {  name: 'global', message: 'hallo'})
 .use(router)
 .mount('#app');
+
+/**
+ * 导航解析流程
+ * --------------------------------------------------------
+ * 导航被触发。
+ * 在失活的组件里调用 beforeRouteLeave 守卫。
+ * 调用全局的 beforeEach 守卫。
+ * 在重用的组件里调用 beforeRouteUpdate 守卫(2.2+)。
+ * 在路由配置里调用 beforeEnter。
+ * 解析异步路由组件。
+ * 在被激活的组件里调用 beforeRouteEnter。
+ * 调用全局的 beforeResolve 守卫(2.5+)。
+ * 导航被确认。
+ * 调用全局的 afterEach 钩子。
+ * 触发 DOM 更新。
+ * 调用 beforeRouteEnter 守卫中传给 next 的回调函数，创建好的组件实例会作为回调函数的参数传入。
+ */
